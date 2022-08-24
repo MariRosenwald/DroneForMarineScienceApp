18f84361e7869612882059a24fe42e25b89ab740
MariRosenwald <45806411+MariRosenwald@users.noreply.github.com>
2022-08-23 17:43:07 -0700

    Worked on Shark's model
    Note that this file is the main file for the project, the other files are for team reference

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.videoplayer import VideoPlayer
 
 
 class OpenScreenGrid(GridLayout):
@@ -46,30 +47,101 @@ class OpenScreenGrid(GridLayout):
         self.date.text = ""
         self.area.text = ""
 
+        sm.current = "WM"
+
 
 class OpenScreen(Screen):
 
     def __init__(self, **kwargs):
         super(OpenScreen, self).__init__(**kwargs)
 
-        # self.add_widget(OpenScreenGrid)
+        self.add_widget(OpenScreenGrid())
 
 
-# class WhichModelScreen(Screen):
-#     def
-# def buttonScreen(GridLayout):
-#     def __init__(self, **kwargs):
-#         super(buttonScreen, self).__init__(**kwargs)
+class WhichModelGrid(GridLayout):
+    def __init__(self, **kwargs):
+        super(WhichModelGrid, self).__init__(**kwargs)
+        self.cols = 1
+
+        self.sharkbtn = Button(text="Shark Model", font_size=40)
+        self.sharkbtn.bind(on_press=self.pressedShark)
+        self.add_widget(self.sharkbtn)
+
+        self.cowbtn = Button(text="Cow Model", font_size=40)
+        self.cowbtn.bind(on_press=self.pressedCow)
+        self.add_widget(self.cowbtn)
+
+        self.backBtn = Button(text="back", font_size=40)
+        self.backBtn.bind(on_press=self.pressedBack)
+        self.add_widget(self.backBtn)
+
+    def pressedShark(self, instance):
+        print("shark")
+
+        sm.current = "SharkModelScreen"
+
+    def pressedCow(self, instance):
+        print("cow")
+
+        sm.current = "OpenScreen"
+
+    def pressedBack(self, instance):
+        print("back")
+
+        sm.current = "OpenScreen"
+
+
+class WhichModelScreen(Screen):
+    def __init__(self, **kwargs):
+        super(WhichModelScreen, self).__init__(**kwargs)
+
+        self.add_widget(WhichModelGrid())
+
+
+class SharkScreenGrid(GridLayout):
+    def __init__(self, **kwargs):
+        super(SharkScreenGrid, self).__init__(**kwargs)
+        self.cols = 1
+
+        self.topGrid = GridLayout()
+        self.topGrid.cols = 1
+
+        # make sure pillow and ffpyplayer
+        self.player = VideoPlayer(source='/Users/marirosenwald/Downloads/sharkvideo.mp4')
+        self.player.state = "play"
+        self.player.allow_stretch = True
+        self.topGrid.add_widget(self.player)
+
+        self.add_widget(self.topGrid)
+
+        self.backBtn = Button(text="back", font_size=40)
+        self.backBtn.bind(on_press=self.pressedBack)
+        self.add_widget(self.backBtn)
+
+    def pressedBack(self, instance):
+        print("back")
+
+        sm.current = "OpenScreen"
+
+class SharkModelScreen(Screen):
+    def __init__(self, **kwargs):
+        super(SharkModelScreen, self).__init__(**kwargs)
+
+        self.add_widget(SharkScreenGrid())
+
+
+
+sm = ScreenManager()
+sm.add_widget(OpenScreen(name="OpenScreen"))
+sm.add_widget(WhichModelScreen(name="WM"))
+sm.add_widget(SharkModelScreen(name="SharkModelScreen"))
+sm.current = "OpenScreen"
 
 
 class CSCApp(App):
 
     def build(self):
-        sm = ScreenManager()
-        sm.add_widget(OpenScreen(name="OpenScreen"))
-        # sm.current_screen()
         return sm
-
 
 if __name__ == "__main__":
     CSCApp().run()
